c81e18d25ed6f24c854eb6166ba068f4b05d007b
lloydlombardi <108779828+lloydlombardi@users.noreply.github.com>
2022-08-11 22:24:47 +0100

    Draft 1 completed

--- Week_05/weekend_homework/shiny_weekend_homework/video_game_app.R
+++ Week_05/weekend_homework/shiny_weekend_homework/video_game_app.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(shiny)
 library(CodeClanData)
 library(plotly)
+library(bslib)
 
 # wrangling ---------------------------------------------------------------
 
@@ -25,11 +26,16 @@ plot_colour <- c("Blue" = "deepskyblue", "Pink" = "deeppink", "Green" = "chartre
 
 point_shape <- c("Square" = "15", "Circle" = "16", "Triangle" = "17")
 
-
+all_publishers <- games %>% 
+  distinct(publisher) %>% 
+  arrange(publisher) %>% 
+  pull()
 
 # ui ----------------------------------------------------------------------
 
 ui <- fluidPage(
+  
+  theme = bs_theme(bootswatch = "superhero"),
   
   sidebarLayout(
     
@@ -40,32 +46,33 @@ ui <- fluidPage(
       br(),
       
       radioButtons(inputId = "rating_choice",
-                   label = "Select a Rating",
+                   label = tags$h2("Select a Rating"),
                    choices =  all_ratings
       ),
       
       selectInput(inputId = "genre_choice",
-                  label = "Choose a Genre",
+                  label = tags$h2("Choose a Genre"),
                   choices =  all_genres
       ),
       
       sliderInput(inputId = "year_choice",
-                  label = "Choose a Year",
+                  label = tags$h2("Choose a Year"),
                   min = min_year,
                   max = max_year,
-                  value = c(min_year, max_year) 
+                  value = c(min_year, max_year),
+                  sep = ""
       ),
       
       fluidRow(
         
-        column(2,
+        column(4,
                actionButton(inputId = "update_scatt",
                             label = "Update Scatter")
         ),
         
-        column(2,
+        column(4,
                actionButton(inputId = "update_line",
-                            label = "Update Line")
+                            label = "Update \n Line")
         )
       )
     ),
@@ -76,15 +83,17 @@ ui <- fluidPage(
         
         column(4,
                radioButtons(inputId = "colour_choice",
-                            label = "Select the point colour",
-                            choices = plot_colour
+                            label = tags$h4("Select the point colour"),
+                            choices = plot_colour,
+                            inline = TRUE
                )
         ),
         
         column(4,
                radioButtons(inputId = "shape_choice",
-                            label = "Select the point shape",
-                            choices = point_shape
+                            label = tags$h4("Select the point shape"),
+                            choices = point_shape,
+                            inline = TRUE
                )
         )
       ),
@@ -96,28 +105,29 @@ ui <- fluidPage(
         ),
         
         tabPanel("Data",
-                 DF::dataTableOutput
+                 DT::dataTableOutput("table_output")
         )
       ),
       
       fluidRow(
         
         column(4,
-               radioButtons(inputId = "",
-                            label = "",
-                            choices = 
+               radioButtons(inputId = "colour_choice_2",
+                            label = tags$h4("Select the line colour"),
+                            choices = plot_colour,
+                            inline = TRUE
                )
         ),
         
         column(4,
-               selectInput(inputId = "",
-                           label = "",
-                           choices = 
+               selectInput(inputId = "publisher_choice",
+                           label = tags$h4("Choose the publisher"),
+                           choices = all_publishers
                )
         )
       ),
       
-      plotOutput()
+      plotOutput("year_v_sales")
       
     )
   )
@@ -127,6 +137,41 @@ ui <- fluidPage(
 # server ------------------------------------------------------------------
 
 server <- function(input, output) {
+  
+  filtered_data_scatter <- eventReactive(input$update_scatt, {
+    games %>% 
+      filter(rating == input$rating_choice,
+             genre == input$genre_choice)
+  })
+  
+  filtered_data_line <- eventReactive(input$update_line, {
+    games %>% 
+      filter(year_of_release > input$year_choice[1] & year_of_release < input$year_choice[2])
+  })
+  
+  output$user_v_critic <- renderPlot({
+    filtered_data_scatter() %>% 
+      ggplot(aes(x = user_score,
+                 y = critic_score)) +
+      geom_point(shape = as.integer(input$shape_choice), colour = input$colour_choice, show.legend = FALSE, size = 3) +
+      labs(x = "User Score",
+           y = "Critic Score",
+           title = "The Relationship Between the User Score and Critic Score")
+  })
+  
+  output$table_output <- DT::renderDataTable({
+    filtered_data_scatter()
+  })
+  
+  output$year_v_sales <- renderPlot({
+    filtered_data_line() %>% 
+      ggplot(aes(x = year_of_release,
+                 y = sales)) +
+      geom_line(colour = input$colour_choice_2, size = 1) +
+      labs(x = "Year of Release",
+           y = "Sales (%)",
+           title = "Sales % vs Year of Release")
+  })
   
 }
 
